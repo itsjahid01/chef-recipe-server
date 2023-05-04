8c2f6a039e30d2589192b415e733b2d454d0a0fd
itsjahid01 <jahidnoman0@gmail.com>
2023-05-05 03:14:33 +0600

    server added

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 3000
+const cors=require('cors');
+const chefRecipes=require('./data/chefRecipes.json')
 
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.send('Hello world!!!!')
 })
+
+app.get('/chefRecipes',(req,res)=>{
+    res.send(chefRecipes)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
